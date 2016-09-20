68bc1032718ba8c799890e5b9ce588c618a380a5
tim <tim@debian>
2016-09-20 13:06:47 -0400

    Made changes during class

--- nn/nn.py
+++ nn/nn.py
@@ -6,9 +6,20 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Load training data
+f = open('../data/trainData.csv')
+print(type(f))
+f.close()
+exit()
+
+f = open('../data/trainLabels.csv')
+train_labels = f.read().split('\n')[:-1]
+f.close()
+
 # Parameters
 learning_rate = 0.001
 training_epochs = 1000 # Num of iterations in back propagation
+batch_size = 1 # Don't divide into batches
 
 hidden_count = 5
 # Network Parameters
@@ -23,7 +34,7 @@ weights = {
     'output_h': tf.Variable(tf.random_uniform(shape=[n_hidden, n_output], minval=-0.5, maxval=0.5)),
 }
 biases = {
-    'hidden_b': tf.Variable(tf.random_uniform(shape=[n_hidden], minval=-0.5, maxval=0.5))),
+    'hidden_b': tf.Variable(tf.random_uniform(shape=[n_hidden], minval=-0.5, maxval=0.5)),
     'output_b': tf.Variable(tf.random_uniform(shape=[n_hidden, n_output], minval=-0.5, maxval=0.5)),
 }
 
@@ -35,7 +46,7 @@ def layer(x):
     return layer
 
 # Build output
-def output(x)
+def output(x):
     out = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['output_h']),
                                    biases['output_b']))
     return out
@@ -46,4 +57,17 @@ y_pred = output(layer(X))
 y_true = tf.placeholder("float", [None, n_output])
 
 # Define loss and optimizer, minimize the squared error
+cost = tf.reduce_mean(tf.pow(y_true - y_pred, 2))
+optimizer = tf.train.RMSPropOptimizer(learning_rate).minimize(cost)
+
+# Initializing the variables
+init = tf.initialize_all_variables()
+
+# Launch the graph
+with tf.Session() as sess:
+    sess.run(init)
+    for epoch in range(training_epochs):
+        _, c = sess.run([optimizer, cost], feed_dict={X: batch_xs, y_true: some_true_value})
+        print("Epoch:", '%04d' % (epoch+1),
+              "cost=", "{:.9f}".format(c))
 
